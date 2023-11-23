4fc8a1ac0b79044f846ba1470879f50c9ab699af
Enei <eneipereira@outlook.com.br>
2023-11-23 18:20:25 -0300

    fix: Altera exercicio 1 / feat: Realiza exercicio 2

--- index.ts
+++ index.ts
@@ -7,11 +7,11 @@ class Car {
   constructor(public type: string, public year: number, public color: string) {}
 }
 
-let car1 = new Car('Corolla', 2021, 'Branco');
-let car2 = new Car('Uno', 2020, 'Bege');
+let carro1 = new Car('Corolla', 2021, 'Branco');
+let carro2 = new Car('Uno', 2020, 'Bege');
 
-console.log(`Type: ${car1.type} - Year: ${car1.year} - Color: ${car1.color}`);
-console.log(`Type: ${car2.type} - Year: ${car2.year} - Color: ${car2.color}`);
+console.log(`Modelo: ${carro1.type} - Ano: ${carro1.year} - Cor: ${carro1.color}`);
+console.log(`Modelo: ${carro2.type} - Ano: ${carro2.year} - Cor: ${carro2.color}`);
 
 /* Herança e Polimorfismo:
 • Crie uma classe base chamada Animal com métodos como emitirSom e mover.
@@ -19,4 +19,43 @@ console.log(`Type: ${car2.type} - Year: ${car2.year} - Color: ${car2.color}`);
 • Sobrescreva o método emitirSom em ambas as classes derivadas.
 • Crie instâncias de Cachorro e Pássaro e chame seus métodos. */
 
-
+class Animal {
+  constructor(public name: string) {}
+ 
+  emitirSom(): void {
+     console.log('O animal emite um som qualquer');
+  }
+ 
+  mover(): void {
+     console.log('O animal está se movendo...');
+  }
+ }
+ 
+ class Cachorro extends Animal {
+  constructor(name: string) {
+     super(name);
+  }
+ 
+  emitirSom(): void {
+     console.log('Au Au Au');
+  }
+ }
+ 
+ class Passaro extends Animal {
+  constructor(name: string) {
+     super(name);
+  }
+ 
+  emitirSom(): void {
+     console.log('Piu-piu');
+  }
+ }
+ 
+ let cachorro = new Cachorro('Bob');
+ let passaro = new Passaro('Juca');
+ 
+ cachorro.emitirSom();
+ passaro.emitirSom();
+ 
+ cachorro.mover();
+ passaro.mover();
